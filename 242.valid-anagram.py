06b3a31421b363866880446c469159ebfb8e950f
Dhinesh L <dhineshdl22@gmail.com>
2023-08-29 22:05:10 +0530

    valid anagram - used array of 26 size

--- 242.valid-anagram.py
+++ 242.valid-anagram.py
@@ -14,19 +14,24 @@ class Solution(object):
         """
         str_len1 = len(s)
         str_len2 = len(t)
+        TOTAL_ALPHABET_COUNT = 26
+        LOWER_ASCII_START_COUNT = 97
         if str_len1 != str_len2:
             return False
         
-        word_map = {}
+        word_count = []
+
+        for i in range(TOTAL_ALPHABET_COUNT):
+            word_count.append(0)
     
         for i in range(str_len1):
-            word_map[s[i]] = word_map.get(s[i],0) + 1
-            word_map[t[i]] = word_map.get(t[i],0) - 1
+            word_count[ord(s[i])-LOWER_ASCII_START_COUNT] += 1
+            word_count[ord(t[i])-LOWER_ASCII_START_COUNT] -= 1
         
-        for k in word_map.keys():
-            if word_map[k] != 0:
+        for k in range(TOTAL_ALPHABET_COUNT):
+            if word_count[k] != 0:
                 return False
-        
+
         return True
 
 
